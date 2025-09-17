28933720f0d22231bf5c058dfe780f34b21c5d52
Raphael Aldaz <raphael.aldaz@gmail.com>
2025-09-17 15:27:58 +0200

    Chore: Creation de la carte des leaderboards

--- src/components/BoardCard/BoardCard.tsx
+++ src/components/BoardCard/BoardCard.tsx
@@ -1,33 +1,58 @@
 import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import like_icon from "../../assets/like_icon.svg";
-
-export const BoardCard = () => {
+interface BoarCardProps {
+  likes_number: number;
+  img: string;
+  description: string;
+}
+export const BoardCard = ({
+  likes_number,
+  img,
+  description,
+}: BoarCardProps) => {
   return (
-    <Card sx={{ display: "flex" }}>
+    <Card
+      sx={{
+        display: "flex",
+        width: { xs: "342px", md: "564px" },
+      }}
+    >
       <CardMedia
         component="img"
         sx={{ width: 151 }}
-        image="https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
-        alt="Live from space album cover"
+        image={img}
+        alt="image card"
       />
-      <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          backgroundColor: "var(--jet)",
+        }}
+      >
         <CardContent
           sx={{
             display: "flex",
-            justifyContent: "space-between",
+            flex: 1,
             alignItems: "center",
+            justifyContent: "space-between",
           }}
         >
-          <Typography>Live From Space</Typography>
           <Typography
-            variant="subtitle1"
-            component="div"
-            sx={{ color: "text.secondary" }}
+            sx={{
+              fontSize: {
+                xs: "14px",
+                md: "16px",
+              },
+            }}
           >
-            234
+            {description}
           </Typography>
-          <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-            <img src={like_icon} alt="" />
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography>{likes_number}</Typography>
+            <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+              <img src={like_icon} alt="like icon" />
+            </Box>
           </Box>
         </CardContent>
       </Box>
--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,5 @@ import "./styles/index.css";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
