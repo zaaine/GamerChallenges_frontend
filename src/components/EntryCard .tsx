5ea12f260cd0438e37e782d3b1c54be7596159c3
zaaine <zaaine@livefr>
2025-09-24 16:08:02 +0200

    fix: pull request actions done

--- src/components/EntryCard .tsx
+++ src/components/EntryCard .tsx
@@ -18,7 +18,6 @@ interface EntryCardProps {
   description: string
   videoUrl?: string
 }
-
 export default function EntryCard({
   image,
   pseudo,
@@ -34,8 +33,8 @@ export default function EntryCard({
     <Box marginTop={4} marginBottom={0}>
       <Card
         sx={{
-          backgroundColor: "#333333",
-          color: "#EBEBFF",
+          backgroundColor: "var(--jet)",
+          color: "var(--lavander)",
           maxWidth: {
             xs: "300px",
             md: "300px",
@@ -46,9 +45,7 @@ export default function EntryCard({
           display="flex"
           justifyContent="space-between"
           alignItems="center"
-          marginRight={2}
-          marginTop={1}
-          marginBottom={-1}
+          margin="16px 16px 0 0"
         >
           <Avatar
             src={image || avatarDefault}
@@ -57,7 +54,7 @@ export default function EntryCard({
               width: 40,
               height: 40,
               padding: 1,
-              bgcolor: "primary.dark",
+              bgcolor: "var(--tropical-indigo)",
               m: 1,
             }}
           />
@@ -74,27 +71,52 @@ export default function EntryCard({
               {pseudo || "Pseudo"}
             </Typography>
           </Box>
-          <Typography variant="body2" marginBottom={2}>
+          <Typography
+            variant="body2"
+            marginBottom={2}
+            sx={{
+              display: "-webkit-box",
+              WebkitLineClamp: { xs: 2, md: 3 }, // Limite de lignes
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
             {description || "Description courte de la vidéo"}
           </Typography>
-          <Box margin="auto" sx={{ width: "100%" }}>
-            <div className="relative pt-[58.25%] w-full cursor-pointer">
-              <div className="absolute top-0 left-0 w-full h-full mb-0">
+          <Box>
+            <Box
+              sx={{
+                position: "relative",
+                paddingTop: "58.25%",
+                width: "100%",
+                cursor: "pointer",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
                 <ReactPlayer
                   src={videoUrl || "https://muxed.s3.amazonaws.com/leds.mp4"}
                   width="100%"
                   height="100%"
                   controls
                 />
-              </div>
-            </div>
+              </Box>
+            </Box>
           </Box>
           <Box display="flex" justifyContent="flex-end" mt={0} mb={-2}>
             <IconButton onClick={toggleLike} aria-label="like">
               {liked ? (
-                <FavoriteIcon sx={{ color: "#8585ff" }} />
+                <FavoriteIcon sx={{ color: "var(--tropical-indigo)" }} />
               ) : (
-                <FavoriteBorderIcon sx={{ color: "#EBEBFF" }} />
+                <FavoriteBorderIcon sx={{ color: "var(--lavander)" }} />
               )}
             </IconButton>
           </Box>
--- src/pages/notFoundPage.tsx
+++ src/pages/notFoundPage.tsx
@@ -1,10 +1,7 @@
-import EntryCard from "../components/EntryCard "
-
 function NotFoundPage() {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <EntryCard />
     </div>
   )
 }
