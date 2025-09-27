5b338f7a02f51c498874c36ef81a007593439959
zaaine <zaaine@livefr>
2025-09-27 11:32:52 +0200

    feat: the submit send the challenge and its correctly formated-The modal have snackbar to informe

--- src/utils/gameTitles.ts
+++ src/utils/gameTitles.ts
@@ -1,6 +1,11 @@
 import GameService from "../services/GameService"
 
-export const fetchGameTitles = async (): Promise<string[]> => {
+export const fetchGameTitles = async (): Promise<
+  { id: number; title: string }[]
+> => {
   const games = await GameService.getAllGames()
-  return games.map((game) => game.title)
+  return games.map((game) => ({
+    id: game.id,
+    title: game.title,
+  }))
 }
